94a2dfa01252e6e477cc806ab734bfec6017046d
yuarmy0921 <christylin890921@gmail.com>
2024-11-14 16:53:52 +0800

    fix indentation

--- Retrieval/qa.py
+++ Retrieval/qa.py
@@ -16,79 +16,79 @@ neo4j.set_chunk_overlap(chunk_overlap)
 reranker = Reranker()
 
 def find_best_document(query: list[float], embeddings: list[list[float]]) -> int:
-        dot_products = np.dot(np.stack(embeddings), query)
-        idx = np.argmax(dot_products)
+	dot_products = np.dot(np.stack(embeddings), query)
+	idx = np.argmax(dot_products)
 
-        return idx
+	return idx
 
 def find_best_document_top_k(query: list[float], embeddings: list[list[float]], k=3) -> int:
-        k = min(k, len(embeddings))
-        dot_products = np.dot(np.stack(embeddings), query)
-        topk = np.argpartition(dot_products, -k)[-k:]
+	k = min(k, len(embeddings))
+	dot_products = np.dot(np.stack(embeddings), query)
+	topk = np.argpartition(dot_products, -k)[-k:]
 
-        return topk
+	return topk
 
 def get_ans(query: list[float], source: list[int]) -> int:
-    num_of_chunks = []
-    embed_doc_search = []
+	num_of_chunks = []
+	embed_doc_search = []
 
-    for s in source:
-        embeddings = neo4j.get_doc_embeddings(question['category'], s)
-        num_of_chunks.append(len(embeddings))
-        embed_doc_search += embeddings
+	for s in source:
+		embeddings = neo4j.get_doc_embeddings(question['category'], s)
+		num_of_chunks.append(len(embeddings))
+		embed_doc_search += embeddings
 
-    idx = find_best_document(query, embed_doc_search)
-    idx += 1
+	idx = find_best_document(query, embed_doc_search)
+	idx += 1
 
-    for i, num in enumerate(num_of_chunks):
-        idx -= num
+	for i, num in enumerate(num_of_chunks):
+		idx -= num
 
-        if idx <= 0:
-            return question['source'][i]
+		if idx <= 0:
+			return question['source'][i]
         
 def get_ans_rerank(category: str, query_text: str, query_embedding: list[float], source: list[int]) -> int:
-    num_of_chunks = []
-    embed_doc_search = []
+	num_of_chunks = []
+	embed_doc_search = []
 
-    for s in source:
-        embeddings = neo4j.get_doc_embeddings(question['category'], s)
-        num_of_chunks.append(len(embeddings))
-        embed_doc_search += embeddings
+	for s in source:
+		embeddings = neo4j.get_doc_embeddings(question['category'], s)
+		num_of_chunks.append(len(embeddings))
+		embed_doc_search += embeddings
 
-    indices = find_best_document_top_k(query_embedding, embed_doc_search)
-    source_candidate = []
+	indices = find_best_document_top_k(query_embedding, embed_doc_search)
+	source_candidate = []
 
-    for idx in indices:
-        idx += 1
-        i = 0
-        while i < len(num_of_chunks):
-            num = num_of_chunks[i]
+	for idx in indices:
+		idx += 1
+		i = 0
+		while i < len(num_of_chunks):
+			num = num_of_chunks[i]
 
-            if idx-num <= 0:
-                source_candidate.append([question['source'][i], idx-1])
-                break
-            else:
-                idx -= num
-                i += 1
-                
-    # source_candidate
-    records = []
+			if idx-num <= 0:
+				source_candidate.append([question['source'][i], idx-1])
+				break
+			else:
+				idx -= num
+				i += 1
+							
+	# source_candidate
+	records = []
 
-    for candidate in source_candidate:
-        text = neo4j.get_chunk_text(category, candidate[0], candidate[1])
-        record = {'id': f'{candidate[0]} {candidate[1]}', 'title': category, 'content': text}
-        records.append(record)
+	for candidate in source_candidate:
+		text = neo4j.get_chunk_text(category, candidate[0], candidate[1])
+		record = {'id': f'{candidate[0]} {candidate[1]}', 'title': category, 'content': text}
+		records.append(record)
 
-    res = reranker.rerank(category, query_text, records)
-    score = 0
-    doc_no = -1
+	res = reranker.rerank(category, query_text, records)
+	score = 0
+	doc_no = -1
 
-    for record in res:
-        if record.score > score:
-            score = record.score
-            doc_no = record.id.split()[0]
+	for record in res:
+		if record.score > score:
+			score = record.score
+			doc_no = record.id.split()[0]
 
-    return int(doc_no)
+	return int(doc_no)
         
 
 parser = argparse.ArgumentParser(description='Retrieve answer')
@@ -97,7 +97,7 @@ parser.add_argument('--pred', type=str, help='Your output prediction json file')
 args = parser.parse_args()
 
 with open(args.question, 'r') as f:
-    questions = json.load(f)['questions']
+  questions = json.load(f)['questions']
 
 queries = [question['query'] for question in questions]
 # with open('query_embeddings_preliminary.json', 'r') as f:
@@ -109,7 +109,7 @@ max_batch_size = 250
 num_batches = num_queries // max_batch_size
 
 for i in range(num_batches):
-    query_embeddings += vertex_ai.get_query_embedding_batch(queries[i*max_batch_size:(i+1)*max_batch_size])
+  query_embeddings += vertex_ai.get_query_embedding_batch(queries[i*max_batch_size:(i+1)*max_batch_size])
 query_embeddings += vertex_ai.get_query_embedding_batch(queries[num_batches*max_batch_size:])
 
 # with open('data/preliminary/questions_example.json', 'r') as f:
@@ -119,21 +119,21 @@ query_embeddings += vertex_ai.get_query_embedding_batch(queries[num_batches*max_
 
 answers = []
 for i, question in enumerate(questions):
-    print('qid:', question['qid'], end=', ')
-    ans = get_ans(query_embeddings[i], question['source'])
-    # ans = get_ans_rerank(
-    #     category=question['category'], 
-    #     query_text=question['query'],
-    #     query_embedding=query_embeddings[i],
-    #     source=question['source']
-    # )
-    print('ans:', ans)
-    answers.append({'qid': question['qid'], 'retrieve': ans})
-    print('------------------------------------')
+	print('qid:', question['qid'], end=', ')
+	ans = get_ans(query_embeddings[i], question['source'])
+	# ans = get_ans_rerank(
+	#     category=question['category'], 
+	#     query_text=question['query'],
+	#     query_embedding=query_embeddings[i],
+	#     source=question['source']
+	# )
+	print('ans:', ans)
+	answers.append({'qid': question['qid'], 'retrieve': ans})
+	print('------------------------------------')
 
 with open(args.pred, 'w') as f:
-    json.dump({'answers': answers}, f)
+  json.dump({'answers': answers}, f)
 
 # with open('data/preliminary/vector-rerank-20000-10000.json', 'w') as f:
-#     json.dump({'answers': answers}, f)
+# 	json.dump({'answers': answers}, f)
 
